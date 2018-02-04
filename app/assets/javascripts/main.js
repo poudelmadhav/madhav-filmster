d1e5f1404029d367f130efe923ec48bf2e36cdb5
Madhav Paudel <poudelmadhav143@gmail.com>
2018-02-04 12:53:56 +0545

    initial get request from TMBb

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,6 +4,36 @@ $(function(){
 	  data: {"query": "superman"}
 	})
 	.done(function(data){
-	  console.log(data);
+	  displayMovies(data);
 	});
-})
+
+	function displayMovies(data) {
+		let htmlString = "";
+		let imageUrl = getBaseImageUrl();
+
+		data["results"].forEach(function(movie) {
+			htmlString += `<img src=${imageUrl}${movie["poster_path"]} />
+							<p>${movie["title"]}</p>
+							<p>${movie["overview"]}</p>`;
+		});
+
+		$("#movies").append(htmlString);
+	}
+
+	function getBaseImageUrl(){
+	  var url = "";
+	  var settings = {
+	    "async": false,
+	    "crossDomain": true,
+	    "url": "https://api.themoviedb.org/3/configuration?api_key=034661908a1116516775711b1e145da1",
+	    "method": "GET",
+	    "headers": {},
+	    "data": "{}"
+	  }
+
+	  $.ajax(settings).done(function (response) {
+	    url = response["images"]["base_url"] + response["images"]["poster_sizes"][3];
+	  });
+	  return url;
+	}
+});
