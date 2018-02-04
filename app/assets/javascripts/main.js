2b858526c34a597daa718f99b15b53ae115c7e2e
Madhav Paudel <poudelmadhav143@gmail.com>
2018-02-04 14:13:43 +0545

    displaying movie based on search query

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,23 +1,35 @@
 $(function(){
-	$.ajax({
-	  url: 'https://api.themoviedb.org/3/search/movie?api_key=034661908a1116516775711b1e145da1',
-	  data: {"query": "superman"}
-	})
-	.done(function(data){
-	  displayMovies(data);
+	let form = $('#movie-search');
+	form.submit(function(e){
+	  e.preventDefault();
+
+	  $.ajax({
+	    url: 'https://api.themoviedb.org/3/search/movie?api_key=034661908a1116516775711b1e145da1',
+	    data: form.serialize()
+	  })
+	  .done(function(data){
+	    displayMovies(data);
+	  });
 	});
 
 	function displayMovies(data) {
+		let container = $("#movies");
+
 		let htmlString = "";
 		let imageUrl = getBaseImageUrl();
 
-		data["results"].forEach(function(movie) {
-			htmlString += `<img src=${imageUrl}${movie["poster_path"]} />
-							<p>${movie["title"]}</p>
-							<p>${movie["overview"]}</p>`;
-		});
+		container.empty();
+		if (data["results"].length == 0) {
+			htmlString = `<div class="alert alert-danger text-center" role="alert">No Data Found!</div>`;
+		} else {
+			data["results"].forEach(function(movie) {
+				htmlString += `<img src=${movie["poster_path"] == null ? "/assets/default_image.jpg" : imageUrl + movie["poster_path"]} />
+								<p>${movie["title"]}</p>
+								<p>${movie["overview"]}</p>`;
+			});
+		}
 
-		$("#movies").append(htmlString);
+		container.append(htmlString);
 	}
 
 	function getBaseImageUrl(){
