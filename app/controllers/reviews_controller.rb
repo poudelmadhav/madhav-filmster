915ba02778ded3ae8135d044b4d462697bcb2daf
Madhav Paudel <poudelmadhav143@gmail.com>
2018-02-13 14:24:54 +0545

    movie show page styled

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!
-  def index
-    @movies = Movie.all
-  end
-
+  
   def create
     @movie = MovieBuilder.new(tmdb_id: params[:tmdb_id]).build!
 
@@ -12,7 +9,7 @@ class ReviewsController < ApplicationController
 
     if @review.save
       flash[:success] = "Review saved!"
-      redirect_to root_path
+      redirect_to movie_path(@movie.id)
     else
       flash[:alert] = "Woops! It seems there was an error."
       redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#index'
+  get 'main' => 'static_pages#index'
 
   get 'movies' => 'movies#index'
 
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   	resources :reviews
   end
   resources :reviews
+
+  root 'movies#index'
 end
