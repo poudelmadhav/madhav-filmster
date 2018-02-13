552b157e3d8954c16959ef29bbe46fb618467a00
Madhav Paudel <poudelmadhav143@gmail.com>
2018-02-13 18:07:52 +0545

    released status removed and link added to websites of movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
-	validates :title, :release_date, :released, :runtime, :popularity, :language, :budget, :average_vote, :vote_count, :tmdb_id, presence: true
+	validates :title, :release_date, :runtime, :popularity, :language, :budget, :average_vote, :vote_count, :tmdb_id, presence: true
 	has_many :reviews
 end
