3ad0ed8a8e057007159488708747c856b0287772
Madhav Paudel <poudelmadhav143@gmail.com>
2018-02-23 20:49:53 +0545

    username login authentication key transferred in respective user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
   validates :username, presence: true
   validates_uniqueness_of :username
   mount_uploader :avatar, AvatarUploader
